a3884bfab3759f98e1267ab427aba43193ac3e47
Kojo Idrissa <kidrissa@mcdermott.com>
2013-09-04 14:33:48 -0500

    finished costCenterFooter function, which adds dictionary with Cost Center totals to the end of each functionalTable

--- costCenterFooter.py
+++ costCenterFooter.py
@@ -12,10 +12,20 @@ def costCenterFooter(functable):
     for i in functable:
         ccList.append(i[1])
     ccUnique = set(ccList)
-    print ccUnique
-    for i in ccUnique:
-        print i
-        
-        
+    
+    footer = []
+    for cc in ccUnique:
+        ccDict = {str(cc):[]}
+        sumDoe = 0
+        sumProj = 0
+        for row in functable:
+            if row[1] == cc:
+                sumDoe = sumDoe + row[-5]
+                sumProj = sumProj + row[-4]
+        ccDict[str(cc)].extend([sumDoe, sumProj])
+        footer.append(ccDict)
+    functable.append(footer)
+    return functable
+
 
     
